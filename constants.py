587c6b563a4a8a2c6703be6f1edb4910f4ddd453
eXstaticYZ <exstaticyz@gmail.com>
2025-04-01 20:01:58 -0700

    First Commit

---
+++ constants.py
@@ -0,0 +1,24 @@
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+ASTEROID_MIN_RADIUS = 20
+ASTEROID_KINDS = 3
+ASTEROID_SPAWN_RATE = 0.8  # seconds
+ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+# Pure red
+RED = (255, 0, 0)
+
+# Pure green
+GREEN = (0, 255, 0)
+
+# Pure blue
+BLUE = (0, 0, 255)
+
+# Solid black
+BLACK = (0, 0, 0)
+
+# Solid white
+WHITE = (255, 255, 255)
+
+# Grey, a mix of all three
+GREY = (128, 128, 128)
